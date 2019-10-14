45c70adb6ff138d795542e9470ae56d9554d6bcf
dinukakavinda <dinukakavinda_rp@hotmail.com>
2019-10-14 21:43:28 +0530

    first commit

---
+++ functions/index.js
@@ -0,0 +1,116 @@
+const functions = require('firebase-functions');
+const {Storage} = require('@google-cloud/storage');
+const os = require('os');
+const path = require('path');
+const cors = require('cors')({origin:true});
+const Busboy = require('busboy');
+const fs = require('fs');
+
+const projectId = 'fyp-test-db';
+
+const gcs = new Storage({
+    projectId: projectId,
+  });
+
+
+
+
+exports.onFileChange = functions.storage.object().onFinalize((object, context) => {
+
+    const bucket = object.bucket;
+    const contentType = object.contentType;
+    const filePath = object.name;
+    console.log('File change detected, funcion execution started');
+
+
+    if(path.basename(filePath).startsWith('renamed-')){
+        console.log('Already renamed the file')
+        return;
+    }
+
+    const destBucket = gcs.bucket(bucket);
+    const tmpFilePath = path.join(os.tmpdir(),path.basename(filePath));
+    const metadata = {contentType:contentType};
+
+    return destBucket.file(filePath).download({
+        destination:tmpFilePath
+    }).then(()=>{
+        return destBucket.upload(tmpFilePath,{
+        destination:'renamed-' + path.basename(filePath),
+        metadata : metadata
+        })
+    });
+});
+
+
+
+exports.onFileDelete = functions.storage.object().onDelete(event => {
+    console.log(event);
+    return;
+});
+
+
+
+exports.onDataAdded = functions.database.ref('/message/{id}').onCreate((snap, context)=>{
+    const data = snap.val();
+    const newData = {
+        msg : snap.key + ' - ' + data.msg.toUpperCase()
+    };
+    console.log(snap.key);
+    return snap.ref.parent.child('copiedData').set(newData);
+    
+});
+
+
+exports.uploadFile = functions.https.onRequest((req,res)=>{
+    cors(req,res,()=>{
+        if(req.method!='POST'){
+            res.status(500).json({
+                message: 'Not valid!'
+            })
+            return;
+        }
+    })
+
+    const busboy = new Busboy({headers:req.headers});
+    let uploadData = null;
+
+    busboy.on('file',(fieldname,file,filename,encoding,mimetype)=>{
+        const filepath = path.join(os.tmpdir(),filename);
+        uploadData = {file:filepath , type:mimetype};
+        file.pipe(fs.createWriteStream(filepath));
+    });
+
+    busboy.on('finish',()=>{
+        const bucket = gcs.bucket('fyp-test-db.appspot.com')
+        bucket.upload(uploadData.file,{
+            uploadType:'media',
+            metadata : {
+                metadata:{
+                    contentType:uploadData.type
+                }
+            }
+        }).then((err,uploadedFile)=>{
+            if(err){
+                return res.status(500).json({
+                    error:err
+                })
+            }
+
+            res.status(200).json({
+                message: 'It worked!'
+            });
+
+        });
+    });
+
+
+    
+});
+
+
+
+
+
+
+
